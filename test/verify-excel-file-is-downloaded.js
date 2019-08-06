b80ba6e48b42fac82cad840ed1d7e5ab29bc911e
Aleks Slavchev <>
2019-08-06 23:18:55 +0300

    Fixed one test, added new test and added a file downlod function 1. Fix it('should check if cftc page return 200') 2. Add it('should download .csv file from cftc page') 3. Add  downloadFile(url, dest) function

--- test/verify-excel-file-is-downloaded.js
+++ test/verify-excel-file-is-downloaded.js
@@ -1,7 +1,11 @@
+import {downloadFile} from '../lib/index.js';
+import fs from 'fs';
+
 var chai = require('chai')
   , chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
+
 
 describe('CFTC repors tests', () => {
     const expect = chai.expect;
@@ -15,8 +19,9 @@ describe('CFTC repors tests', () => {
             throw err;
         });
     });
-    it.only('should check if cftc page return 200', done => {
-        chai.request('https://www.cftc.gov/dea/newcot/deafut.txt').get('/')
+    
+    it('should check if cftc page return 200', done => {
+        chai.request('https://www.cftc.gov/').get('dea/newcot/deafut.txt')
         .then(function (res) {
             expect(res).to.have.status(200);
             done();
@@ -24,5 +29,24 @@ describe('CFTC repors tests', () => {
         .catch(function (err) {
             throw err;
         });
+    });
+
+    it('should download .csv file from cftc page', done => {
+        const url = "https://www.cftc.gov/dea/newcot/deafut.txt";
+        const destination = "cftcTest1.txt";
+        
+        downloadFile(url,destination)
+        .then(function (res) {
+            const fileExists = fs.existsSync(destination);
+            expect(fileExists).to.be.true;
+            done();
+        })
+        .catch(function (err) {
+            throw err;
+        });
+    
+    // TODO: Verify if file.size > 1 bites
+    // TODO: Verify fs documentation how to read the downloaded file and manipulate its
+
     });
   });
